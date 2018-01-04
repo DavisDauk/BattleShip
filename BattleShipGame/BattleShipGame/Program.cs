7c110223934136da139b0b26d694f15df390a89f
crunch2001 <35105059+crunch2001@users.noreply.github.com>
2018-01-04 15:00:05 +0000

    GridDisplay

--- BattleShipGame/BattleShipGame/Program.cs
+++ BattleShipGame/BattleShipGame/Program.cs
@@ -10,6 +10,26 @@ namespace BattleShipGame
     {
         static void Main(string[] args)
         {
+            int grid_x = 10;
+            int grid_y = 10;
+            string[,] grid_object = new string[grid_x,grid_y]; 
+            for (int x = 0; x < grid_x; x++)
+            {
+                for (int y = 0; y < grid_y; y++)
+                {
+                    grid_object[x, y] = "~";
+                }
+            }
+            for (int x = 0; x < grid_x; x++)
+            {
+                for (int y = 0; y < grid_y; y++)
+                {
+                    Console.Write("|" + grid_object[x, y] + "|");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+
         }
     }
 }
