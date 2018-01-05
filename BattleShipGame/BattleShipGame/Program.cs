817a5554e51769c565b820dec87bb9abff4c8141
crunch2001 <35105059+crunch2001@users.noreply.github.com>
2018-01-05 09:54:13 +0000

    shooting
    n/a

--- BattleShipGame/BattleShipGame/Program.cs
+++ BattleShipGame/BattleShipGame/Program.cs
@@ -10,26 +10,41 @@ namespace BattleShipGame
     {
         static void Main(string[] args)
         {
-            int grid_x = 10;
-            int grid_y = 10;
+            int grid_x = 11;
+            int grid_y = 11;
             string[,] grid_object = new string[grid_x,grid_y]; 
-            for (int x = 0; x < grid_x; x++)
+            for (int x = 1; x < grid_x; x++)
             {
-                for (int y = 0; y < grid_y; y++)
+                for (int y = 1; y < grid_y; y++)
                 {
                     grid_object[x, y] = "~";
                 }
             }
-            for (int x = 0; x < grid_x; x++)
+            Display(grid_x,grid_y,grid_object);
+        }
+        static void Display(int grid_x, int grid_y, string[,] grid_object)
+        {
+            for (int x = 1; x < grid_x; x++)
             {
-                for (int y = 0; y < grid_y; y++)
+                for (int y = 1; y < grid_y; y++)
                 {
                     Console.Write("|" + grid_object[x, y] + "|");
                 }
                 Console.WriteLine("");
             }
-            Console.ReadLine();
-
+            Shoot(grid_x, grid_y, grid_object);
+        }
+        static void Shoot(int grid_x, int grid_y, string[,] grid_object)
+        {
+            int shoot_x;
+            int shoot_y;
+            Console.WriteLine("x coordinate");
+            shoot_x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y coordinate");
+            shoot_y = Convert.ToInt32(Console.ReadLine());
+            grid_object[shoot_x, shoot_y] = "X";
+            Display(grid_x, grid_y, grid_object);
+            Console.Clear();
         }
     }
 }
