a45f57e810bcca430f7d34bd20d4ac99fedf2df7
DavisDauk <dauksts30@gmail.com>
2018-01-15 16:02:39 +0000

    Update

--- BattleShipGame/BattleShipGame/Program.cs
+++ BattleShipGame/BattleShipGame/Program.cs
@@ -10,30 +10,40 @@ namespace BattleShipGame
     {
         static void Main(string[] args)
         {
-            int grid_x = 11;
-            int grid_y = 11;
-            string[,] grid_object = new string[grid_x,grid_y]; 
-            for (int x = 1; x < grid_x; x++)
+            var grid_x = 11;
+            var grid_y = 11;
+            var grid_object = new string[grid_x, grid_y];
+            for (var x = 1; x < grid_x; x++)
             {
-                for (int y = 1; y < grid_y; y++)
+                for (var y = 1; y < grid_y; y++)
                 {
                     grid_object[x, y] = "~";
                 }
             }
-            Display(grid_x,grid_y,grid_object);
+
+            Display(grid_x, grid_y, grid_object);
         }
-        static void Display(int grid_x, int grid_y, string[,] grid_object)
+
+        static void Display(int gridX, int gridY, string[,] gridObject)
         {
-            for (int x = 1; x < grid_x; x++)
+            for (var x = 1; x < gridX; x++)
             {
-                for (int y = 1; y < grid_y; y++)
+                for (var y = 1; y < gridY; y++)
                 {
-                    Console.Write("|" + grid_object[x, y] + "|");
+                    Console.Write("|" + gridObject[x, y] + "|");
                 }
+
+                DrawShip(1, 4, gridObject);
                 Console.WriteLine("");
+
             }
-            Shoot(grid_x, grid_y, grid_object);
+
+
+
+            Shoot(gridX, gridY, gridObject);
         }
+
+
         static void Shoot(int grid_x, int grid_y, string[,] grid_object)
         {
             int shoot_x;
@@ -46,5 +56,92 @@ namespace BattleShipGame
             Display(grid_x, grid_y, grid_object);
             Console.Clear();
         }
+
+        static string[,] DrawShip(int direction, int shipLength, string[,] grid)
+        {
+            var noShips = false;
+
+            if (direction == 1) //vertical
+            {
+                var shipsY = RandomNumber(0, 9 - shipLength);
+                var shipsX = RandomNumber(0, 9);
+
+                noShips = CheckForShips(1, shipsX, shipsY, grid, shipLength);
+                if (noShips)
+                {
+                    for (var i = 0; i < shipLength; i++)
+                    {
+                        grid[shipsX, shipsY++] = "v";
+                    }
+                }
+                else
+                {
+                    DrawShip(direction, shipLength, grid);
+                }
+
+            }
+            else //Horizontal
+            {
+                var shipsX = RandomNumber(0, 10 - shipLength);
+                var shipsY = RandomNumber(0, 10);
+
+                noShips = CheckForShips(0, shipsX, shipsY, grid, shipLength);
+
+                if (noShips)
+                {
+                    for (var i = 0; i < shipLength; i++)
+                    {
+                        grid[shipsX++, shipsY] = "s";
+                    }
+                }
+                else
+                {
+                    CheckForShips(0, shipsX, shipsY, grid, shipLength);
+                }
+            }
+
+            return grid;
+        }
+
+        public static int RandomNumber(int min, int max)
+        {
+           var ran = new Random();
+           var number = ran.Next(min, max);
+           return number;
+        }
+
+        static bool CheckForShips(int direction, int positionX, int positionY, string[,] grid, int shipLength)
+        {
+            if (direction == 1) //vertical
+            {
+                for (var i = positionX; i < positionX + shipLength; i++)
+                {
+                    if (grid[i, positionY] != "~")
+                    {
+                        return false;
+                    }
+                }
+            }
+            else //Horizontal
+            {
+                for (var i = positionY; i < shipLength + positionY; i++)
+                 {
+                  if (grid[positionX, i] != "~")
+                      {
+                           return false;
+                       }
+
+
+                    }
+                }
+            
+
+            return true;
+        }
     }
 }
+
+                
+             
+         
+                 
